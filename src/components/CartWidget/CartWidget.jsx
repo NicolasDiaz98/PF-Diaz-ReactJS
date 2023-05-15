a1f03793f6c1afc2205df7b5b7f14ddaf86e182f
NicolasDiaz98 <nicolasdiaz98@hotmail.com>
2023-05-15 19:57:33 -0300

    Avanzando

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,18 @@
 import './CartWidget.css'
+import { useContext } from 'react'
 import cart from './assets/Cart.png'
+import { CartContext } from '../../context/CartContext'
+import { Link } from 'react-router-dom'
 
 const CartWidget = () => {
+  
+  const { totalQuantity } = useContext(CartContext)
+
   return (
-    <div className='CartWidget'>
-        <img src={cart} alt="cart-widget" />
-        0
-    </div>
+    <Link to='/cart' className='CartWidget' style={{ display: totalQuantity > 0 ? 'block' : 'none'}}>
+      <img className='CartImg' src={cart} alt="Cart" />
+      { totalQuantity }
+    </Link>
   )
 }
 
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,25 @@
 import './ItemDetail.css'
 import ItemCount from "../ItemCount/ItemCount"
+import { useContext, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { CartContext } from '../../context/CartContext'
 
 const ItemDetail = ({ id, name, img, category, description, price, stock}) => {
+
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    const { addItem } = useContext(CartContext)
+
+    const handleOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+
+        const item = {
+            id, name, price
+        }
+
+        addItem(item, quantity)
+    }
+
   return (
 
         <div className="CardItem">
@@ -25,7 +43,13 @@ const ItemDetail = ({ id, name, img, category, description, price, stock}) => {
                 </p>
             </section>
             <footer className="ItemFooter">
-                <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log('Cantidad agregada',quantity)} />
+                {
+                    quantityAdded > 0 ? 
+                        (<Link to='/cart' className='Option'>Finalizar compra</Link>)
+                        :
+                        (<ItemCount initial={1} stock={stock} onAdd={handleOnAdd} />)
+                }
+                
             </footer>
         </div>
   )
